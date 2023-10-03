d98377b72b9e2e03ac8c0279ac4010846cb376fe
BorseHaraldsen <Augustborse@gmail.com>
2023-10-03 09:43:57 +0200

    Mark the PartsRepository as a component with explanation.

--- src/main/java/no/borse/firstspringboot/PartsRepository.java
+++ src/main/java/no/borse/firstspringboot/PartsRepository.java
@@ -1,4 +1,13 @@
 package no.borse.firstspringboot;
 
+import org.springframework.stereotype.Component;
+
+// NEEDS to be marked for Spring. If not, it will not be found.
+// Can call it other ways:
+/* @Bean
+*  @Service
+*  @Repository
+*  They all are basically saying Component. */
+@Component
 public class PartsRepository {
 }
