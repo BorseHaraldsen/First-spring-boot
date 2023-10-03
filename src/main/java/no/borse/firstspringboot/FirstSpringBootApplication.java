516a91b7b444adc82689cb3a4eb46a1d11ae1c5a
BorseHaraldsen <Augustborse@gmail.com>
2023-10-03 09:35:03 +0200

    Initial spring project. Also addition of the class PartsController.

---
+++ src/main/java/no/borse/firstspringboot/FirstSpringBootApplication.java
@@ -0,0 +1,17 @@
+package no.borse.firstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* Annotations: Tie into Java reflections.
+* A way for Java to look at its own running code, while it is running.
+*  */
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/borse/firstspringboot/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package no.borse.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
